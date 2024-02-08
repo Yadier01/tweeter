dec3c20c8a0e7628006303e38a45492263e9aeb5
Yadier <yadierg7@gmail.com>
2024-02-08 16:19:27 -0400

    Add TweetComment component

---
+++ src/app/_components/TweetComment.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useState } from "react";
+import { CommentStructure } from "./CommentStructure";
+
+interface Reply {
+  id: number;
+  name: string;
+  postId: number;
+  parentId: number | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface Comment extends Reply {
+  replies?: Reply[];
+}
+
+type Props = {
+  comment: Comment[];
+  parentPostId: number;
+};
+export const TweetComment = ({ comment, parentPostId }: Props) => {
+  const [replyTo, setReplyTo] = useState<number | null>(null);
+  const handleAdd = (id: number) => {
+    setReplyTo(id);
+  };
+
+  const topLevelComments = comment.filter((com) => com.parentId === null);
+
+  return (
+    <>
+      {topLevelComments.map((com: any) => (
+        <CommentStructure
+          key={com.id}
+          com={com}
+          parentPost={parentPostId}
+          handleAdd={handleAdd}
+          replyTo={replyTo}
+        />
+      ))}
+    </>
+  );
+};
