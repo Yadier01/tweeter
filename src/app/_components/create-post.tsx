20a37c996954087c400c24f0a63985642fd3f519
Yadier <yadierg7@gmail.com>
2024-02-08 16:20:06 -0400

    Refactor CreatePost component

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -1,43 +1,55 @@
 "use client";
-
-import { useRouter } from "next/navigation";
 import { useState } from "react";
-
+import { useRouter } from "next/navigation";
+import { BiWorld } from "react-icons/bi";
 import { api } from "~/trpc/react";
 
 export function CreatePost() {
   const router = useRouter();
-  const [name, setName] = useState("");
+  const [postContent, setPostContent] = useState("");
 
-  const createPost = api.post.create.useMutation({
+  const createPostMutation = api.post.create.useMutation({
     onSuccess: () => {
       router.refresh();
-      setName("");
+      setPostContent("");
     },
   });
 
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createPostMutation.mutate({ name: postContent });
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPostContent(e.target.value);
+  };
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        createPost.mutate({ name });
-      }}
-      className="flex flex-col gap-2"
-    >
-      <input
-        type="text"
-        placeholder="Title"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        className="w-full rounded-full px-4 py-2 text-black"
-      />
-      <button
-        type="submit"
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-        disabled={createPost.isLoading}
-      >
-        {createPost.isLoading ? "Submitting..." : "Submit"}
-      </button>
+    <form onSubmit={handleFormSubmit} className="flex w-full flex-col gap-2">
+      <div className="flex w-full flex-col justify-between p-2">
+        <textarea
+          name="tweet"
+          onChange={handleInputChange}
+          value={postContent}
+          id="tweet"
+          placeholder="What's happening?"
+          className="w-full"
+        />
+        <div className="flex justify-between">
+          <ul className="flex items-center gap-3">
+            <li>img</li>
+            <li className="flex items-center text-[#2F80ED]">
+              <BiWorld /> Everyone can reply
+            </li>
+          </ul>
+
+          <button
+            className="h-[32px] w-[81px] rounded-[4px] bg-[#2F80ED] text-sm text-white"
+            disabled={createPostMutation.isLoading}
+          >
+            Tweet
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
