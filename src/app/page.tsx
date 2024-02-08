dbec7036162cbe19e3074e0beb4d32a3d50f97c5
Yadier <yadierg7@gmail.com>
2024-02-08 16:20:29 -0400

    Add react-icons and zustand dependencies, update Post and Comment models

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,27 +12,10 @@ export default async function Home() {
 
   return (
     <main className=" flex min-h-screen flex-col items-center bg-[#F2F2F2] text-white">
-      <div className="ax-w-2xl flex w-[754px] flex-col items-center ">
+      <div className=" my-8 flex w-[754px] flex-col items-center gap-10 ">
         <TweetSomething />
         <Tweet />
-        {/* <CrudShowcase /> */}
       </div>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
