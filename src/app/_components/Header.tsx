fb6d845727a54c76e968e6bc8f4519e5c98f9981
Yadier <yadierg7@gmail.com>
2024-02-11 11:19:49 -0400

    Add popover component to header

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,9 +1,18 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-
+import { FaCircleUser } from "react-icons/fa6";
+import { MdGroup } from "react-icons/md";
+import { IoSettingsSharp } from "react-icons/io5";
+import { HiLogout } from "react-icons/hi";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
 export const Header = () => {
   const [active, setActive] = useState(0);
+  const [activePopover, setActivePopover] = useState(true);
   const links = ["Home", "Explore", "Bookmarks"];
 
   return (
@@ -25,10 +34,33 @@ export const Header = () => {
           ))}
         </ul>
       </nav>
-      <div className="flex items-center gap-3">
-        <img src="/pfp.jpg" alt="" className="h-10  rounded-full " />
-        <p>Xanthe Neal</p>
-      </div>
+      <Popover>
+        <PopoverTrigger>
+          <div className="flex items-center gap-3">
+            <img src="/pfp.jpg" alt="" className="h-10  rounded-full " />
+
+            <p>Xanthe Neal</p>
+          </div>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="flex flex-col gap-3">
+            <span className="flex flex-col gap-2 border-b pb-4  ">
+              <p className="flex items-center gap-3 rounded-md p-2 transition hover:bg-[#F2F2F2]">
+                <FaCircleUser /> My Profile
+              </p>
+              <p className="flex items-center gap-3 rounded-md p-2 transition hover:bg-[#F2F2F2]">
+                <MdGroup /> Group Chat
+              </p>
+              <p className="flex items-center gap-3 rounded-md p-2 transition hover:bg-[#F2F2F2]">
+                <IoSettingsSharp /> Settings
+              </p>
+            </span>
+            <p className="flex items-center gap-3 rounded-md p-2 text-[#EB5757] transition hover:bg-[#F2F2F2]">
+              <HiLogout /> Logout
+            </p>
+          </div>
+        </PopoverContent>
+      </Popover>
     </header>
   );
 };
