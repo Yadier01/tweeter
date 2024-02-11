5a86355833c27e7f2322e00054f9690d78ab1d33
Yadier <yadierg7@gmail.com>
2024-02-11 17:51:36 -0400

    Refactor TweetSomething component to fetch and display user name

--- src/app/_components/TweetSomething.tsx
+++ src/app/_components/TweetSomething.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
 import { BiWorld } from "react-icons/bi";
 import { CreatePost } from "./create-post";
-export const TweetSomething = () => {
+import { api } from "~/trpc/react";
+import cookie from "js-cookie";
+import { userStore } from "../Store/userStore";
+import { use, useEffect, useState } from "react";
+export const TweetSomething = ({
+  username,
+}: {
+  username?: string | undefined;
+}) => {
+  let getUserName: any;
+  const setUserName = userStore((state) => state.setUserName);
+  const token = cookie.get("token")!;
+
+  if (token !== undefined) {
+    getUserName = api.user.getUser.useQuery({ token }).data;
+    setUserName(getUserName?.name);
+  }
   return (
     <div className="mt-1 flex w-full flex-col gap-3 rounded-lg bg-white p-4 text-black">
       <h4 className=" border-b  font-semibold">Tweet something</h4>
